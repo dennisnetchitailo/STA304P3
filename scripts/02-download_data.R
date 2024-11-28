ae9c9732c3ea8a9d75d65b59c651ff33528a4664
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-11-28 00:47:03 -0500

    added accidentally deleted raw file added process to download the datset, manually and automatically

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,21 +1,58 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from tacookson's GitHub repository.
+# Author: Dennis Netchitailo
+# Date: 27 November 2024
+# Contact: dennis.netchitailo@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: Download the "usethis" and "readr" libraries
 # Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+
+#install.packages("usethis")
+#install.packages("readr")
+
+library(usethis)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
+
+#install.packages("usethis")
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+## ___ *Method #1 : Manual* ## ___
+
+# Go to the GitHub repository and manually download the .zip file 
+# Use the following link: https://github.com/tacookson/data
+# Location and open the .zip file
+
+# (1) Locate and move the "bombings.csv" file to the raw_data folder in the 
+# project following the path: data/01-raw_data
+
+# (2) Locate and move the "casualties.csv" file to the raw_data folder in the 
+# project following the path: data/01-raw_data
+
+## ___ Method #2 : Automatic* ## ___
+
+# Choose a directory on your computer to be the location of the .zip file
+# Insert the path of the directory where you want to save the .zip file
+#source_folder = ""
+source_folder = "C:\\Users\\Dennis Netchitailo\\Downloads"
+
+
+# Download the .zip file
+
+usethis::use_course(
+  url = 'https://github.com/tacookson/data/archive/refs/heads/master.zip',
+  destdir = destination)
+0
+source_csv_bombings <- file.path(source_folder, "bombings.csv")
+source_csv_casualties <- file.path(source_folder, "casualties.csv")
+
+#target_csv <- "data\\01-raw_data\\raw_data.csv"
+target_csv_bombings <- "C:\Users\Dennis Netchitailo\Documents\STA304P3\data\01-raw_data\raw_data.csv"
+target_csv_casualties <- "C:\Users\Dennis Netchitailo\Documents\STA304P3\data\01-raw_data\raw_data.csv"
 
 
 #### Save data ####
