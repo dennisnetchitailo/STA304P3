071f0e228194a97c63284b998bf58353c97f6ed2
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-11-29 15:41:16 -0500

    improved paths, corrected directory location, added csv reading, removed unnecessary code

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -31,28 +31,25 @@ library(readr)
 ## ___ Method #2 : Automatic* ## ___
 
 # Choose a directory on your computer to be the location of the .zip file
-# Insert the pat 1945h of the directory where you want to save the .zip file
+# Insert the path of the directory where you want to save the .zip file
 #source_folder = ""
-source_folder = "C:\\Users\\Dennis Netchitailo\\Downloads"
+source_folder = "C:/Users/Dennis Netchitailo/Downloads/data-master/britain-bombing-ww2"
 
 
 # Download the .zip file
 
 usethis::use_course(
   url = 'https://github.com/tacookson/data/archive/refs/heads/master.zip',
-  destdir = destination)
+  destdir = source_folder)
 
 source_csv_bombings <- file.path(source_folder, "bombings.csv")
-source_csv_casualties <- file.path(source_folder, "casualties.csv")
+target_csv_bombings <- read.csv(source_csv_bombings) # Read the CSV file into R
 
-#target_csv <- "data\\01-raw_data\\raw_data.csv"
-target_csv_bombings <- "C:\Users\Dennis Netchitailo\Documents\STA304P3\data\01-raw_data\raw_data.csv"
-target_csv_casualties <- "C:\Users\Dennis Netchitailo\Documents\STA304P3\data\01-raw_data\raw_data.csv"
+source_csv_casualties <- file.path(source_folder, "casualties.csv")
+target_csv_casualties <- read.csv(source_csv_casualties) # Read the CSV file into R
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv(target_csv_bombings, "data/01-raw_data/bombings_data.csv") 
+write_csv(target_csv_casualties, "data/01-raw_data/casualties_data.csv")
          
