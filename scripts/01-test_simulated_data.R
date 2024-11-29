ad48800d32bbed638b29ad5cf51fecda5bfb5e03
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-11-29 15:49:09 -0500

    improved formatting to make it clearer where new tests are being conducted removed duplicate test and unneeded code line

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -26,21 +26,21 @@ if (exists("analysis_data")) {
 
 #### Test data ####
 
-# Check if the dataset has 32514 rows
+## Check if the dataset has 32514 rows ##
 if (nrow(analysis_data) == 32514) {
   message("Test Passed: The dataset has 32514 rows.")
 } else {
   stop("Test Failed: The dataset does not have 32514 rows.")
 }
 
-# Check if the dataset has 4 columns
+## Check if the dataset has 4 columns ##
 if (ncol(analysis_data) == 4) {
   message("Test Passed: The dataset has 4 columns.")
 } else {
   stop("Test Failed: The dataset does not have 4 columns.")
 }
 
-# Check if the 'location' column contains only valid locations of England's cities
+## Check if the 'location' column contains only valid locations of England's cities ##
 valid_cities <- c("London", "Manchester", "Liverpool", "Birmingham", "Coventry")
 
 if (all(analysis_data$location %in% valid_cities)) {
@@ -49,14 +49,10 @@ if (all(analysis_data$location %in% valid_cities)) {
   stop("Test Failed: The 'location' column contains invalid city names.")
 }
 
-# Check no negative casualties figures
+## Check no negative casualties figures ##
 all(analysis_data$casualties >= 0) == TRUE
 
-# Check date range
-all(simulated_bombing_data$date >= as.Date("1939-09-01") & 
-      simulated_bombing_data$date <= as.Date("1940-05-10")) == TRUE
-
-# Check if there are any missing values in the dataset
+## Check if there are any missing values in the dataset ##
 if (all(!is.na(analysis_data))) {
   message("Test Passed: The dataset contains no missing values.")
 } else {
@@ -72,8 +68,8 @@ if (all(analysis_data$incident_id != "" & analysis_data$location != "" &
 } else {
   stop("Test Failed: There are empty strings in one or more columns.")
 }
-#analysis_data$incident_id != "" & analysis_data$date != "" &
 
+## Check all dates are within the set range ##
 # Define expected date range
 start_date <- as.Date("1939-09-01")
 end_date <- as.Date("1940-05-10")
@@ -91,7 +87,7 @@ if (n_distinct(analysis_data$location) >= 2) {
   stop("Test Failed: The 'location' column contains less than two unique values.")
 }
 
-# Check for IQR outliers
+## Check for IQR outliers ##
 outliers <- analysis_data %>%
   filter(casualties > 17) 
 if (nrow(outliers) == 0) {
@@ -100,14 +96,14 @@ if (nrow(outliers) == 0) {
   warning("Outliers detected in 'casualties':", nrow(outliers))
 }
 
-# Check all incident IDs are unique
+## Check all incident IDs are unique ##
 if (n_distinct(analysis_data$incident_id) == nrow(analysis_data)) {
   message("Test Passed: All 'incident_id' values are unique.")
 } else {
   stop("Test Failed: Duplicate 'incident_id' values found.")
 }
 
-# Check no missing date values
+## Check no missing date values ##
 if (all(!is.na(analysis_data$date))) {
   message("Test Passed: No missing dates in the dataset.")
 } else {
