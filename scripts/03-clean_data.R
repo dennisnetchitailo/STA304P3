28bece8ec4798f96714ad39a0ce154190711e5c1
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-11-29 22:14:36 -0500

    reoordered code cleaned bombings location var added count days for bombings date vars

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -18,7 +18,7 @@ library(tidyverse)
 # Copy and insert the path to the folder
 #source_folder = "" #Insert path here
 source_folder = "C:/Users/Dennis Netchitailo/Documents/STA304P3"
-#cobined_path <- file.path(source_folder, "data/01-raw_data/bombings.csv")
+#combined_path <- file.path(source_folder, "data/01-raw_data/bombings.csv")
 
 load_csv <- function(file_name, base_folder = source_folder) {
   file_path <- file.path(base_folder, file_name)
@@ -29,24 +29,21 @@ load_csv <- function(file_name, base_folder = source_folder) {
 #### Load Datasets ####
 
 # Load bombings_data.csv
-bombings_data <- load_csv("data/01-raw_data/bombings_data.csv")
+cleaned_bombings_data <- load_csv("data/01-raw_data/bombings_data.csv")
 
 # Load casualties_data.csv
 cleaned_casualties_data <- load_csv("data/01-raw_data/casualties_data.csv")
 
-### BOMBINGS Dataset ###
+#### 
+### CASUALTIES Dataset ###
 
-## Add new column ## 
+## Add new columns ## 
 
 # Add a new column for the proportion of killed relative to total casualties
 cleaned_casualties_data$proportion_killed <- 
   cleaned_casualties_data$killed / cleaned_casualties_data$total_casualties
-
 # Handle cases where total_casualties is zero to avoid division by zero
 cleaned_casualties_data$proportion_killed[cleaned_casualties_data$total_casualties == 0] <- NA
-
-# Preview the updated dataset
-head(cleaned_casualties_data)
 
 # Create dummy variable for Killed
 cleaned_casualties_data$has_killed <- ifelse(cleaned_casualties_data$killed > 0, 1, 0)
@@ -57,38 +54,21 @@ cleaned_casualties_data$has_injured <- ifelse(cleaned_casualties_data$injured > 
 # Create dummy variable for Casualties
 cleaned_casualties_data$has_casualties <- ifelse(cleaned_casualties_data$total_casualties > 0, 1, 0)
 
+### BOMBINGS Dataset ###
+
+# Remove whitespace before/after 
+cleaned_bombings_data$location <- trimws(cleaned_bombings_data$location)
+
+# Duration of bombing in days 
+cleaned_bombings_data$duration_days <- 
+  as.Date(cleaned_bombings_data$end_date) - as.Date(cleaned_bombings_data$start_date)
+
+
+
+
 #### Save data ####
 write_csv(cleaned_casualties_data, "data/02-analysis_data/analysis_data_casualties.csv")
 
-
-
-# ______________________________________________________________________________
-
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+## Save as parquet ##
 
 
