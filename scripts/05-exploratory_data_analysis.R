f4d64d244833dfeaa73a200217e8c6db79eccf89
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-11-30 18:36:40 -0500

    added summary stats for bombings & casualties datasets

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -16,10 +16,74 @@
 library(tidyverse)
 library(rstanarm)
 
+source_folder = "C:/Users/Dennis Netchitailo/Documents/STA304P3"
+
+load_csv <- function(file_name, base_folder = source_folder) {
+  file_path <- file.path(base_folder, file_name)
+  data <- read.csv(file_path)
+  return(data)
+}
+
+#### Load Datasets ####
+
+# Load casualties_data.csv
+casualties_analysis_data <- load_csv("data/02-analysis_data/analysis_data_casualties.csv")
+
+# Load bombings_data.csv
+bombings_analysis_data <- load_csv("data/02-analysis_data/analysis_data_bombings.csv")
+
+
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+#analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
 
 ### Model data ####
+
+## CASUALTIES DATASET ##
+
+# Sum Killed
+sum(casualties_analysis_data$killed, na.rm = TRUE)
+
+# Sum Injured
+sum(casualties_analysis_data$injured, na.rm = TRUE)
+
+# Sum Casualties
+sum(casualties_analysis_data$total_casualties, na.rm = TRUE)
+
+# Sum of difference between Casualties and Killed & Injured
+sum(casualties_analysis_data$total_casualties, na.rm = TRUE) - 
+  (sum(casualties_analysis_data$killed, na.rm = TRUE)
+ + sum(casualties_analysis_data$injured, na.rm = TRUE))
+
+
+## BOMBINGS DATASET ##
+
+# Count Countries
+bombings_analysis_data %>%
+  count(country)
+
+# Earliest and latest bombing dates
+range(bombings_analysis_data$start_date, na.rm = TRUE)
+
+# Bombings per year
+bombings_analysis_data %>%
+  count(start_year)
+
+# Bombings per month (seasonal analysis)
+bombings_analysis_data %>%
+  count(start_month)
+
+# Number of bombings by civil defense region
+bombings_analysis_data %>%
+  count(civil_defence_region)
+
+# Count bombings with missing coordinates
+sum(is.na(bombings_analysis_data$lon) | is.na(bombings_analysis_data$lat))
+
+summary(bombings_analysis_data$duration_days)
+
+
+# __________________________________________________________________
+
 first_model <-
   stan_glm(
     formula = flying_time ~ length + width,
