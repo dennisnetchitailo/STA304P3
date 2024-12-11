d012d8ca51d7a7c9d678b77fc8c2843eaf7a429a
dennisnetchitailo <156948879+dennisnetchitailo@users.noreply.github.com>
2024-12-11 14:36:32 -0500

    deleted unneeded sketches

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -23,7 +23,7 @@ library(brms)
 library(broom.mixed)
 
 #### Read data ####
-model_data <- read_csv("data/analysis_data/combined_data")
+model_data <- read_csv("data/02-analysis_data/combined_data.csv")
 
 
 ### Model data ####
@@ -31,7 +31,7 @@ first_model <- brm(
   total_casualties ~ 
     year + month + time_binary + time_binary + lethality_category + country + civil_defense_region + 
     (1 | casualty_group) + (1 | civil_defense_region),
-  data = combined_data,
+  data = model_data,
   family = zero_inflated_negbinomial(),
   prior = c(
     prior(normal(0, 1), class = "b"),
@@ -48,5 +48,26 @@ saveRDS(
   first_model,
   file = "models/first_model.rds"
 )
+#_______________________________________________________________________________
+### Model data ####
+second_model <- brm(
+  total_casualties ~ 
+    year + month + time_binary + time_binary + lethality_category + country + civil_defense_region + 
+    (1 | casualty_group) + (1 | civil_defense_region),
+  data = model_data,
+  family = zero_inflated_negbinomial(),
+  prior = c(
+    prior(normal(0, 1), class = "b"),
+    prior(exponential(1), class = "sd")
+  ),
+  chains = 5,
+  cores = 14,  # Use 14 cores for faster computation
+  iter = 100
+)
 
 
+#### Save model ####
+saveRDS(
+  second_model,
+  file = "models/second_model.rds"
+)
